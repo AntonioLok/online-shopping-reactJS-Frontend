f1eb2a9af4c1b530362d0358a979b81c26eb3e09
Antonio <antonio.lok.chan@adp.com>
2019-06-24 14:31:46 -0400

    refactor form component into HOC and improve UI for registerm form

---
+++ src/components/common/form/index.js
@@ -0,0 +1,62 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from './button';
+import TextInput from './input-fields/text-input';
+import SelectInput from './input-fields/select-input';
+
+class CustomForm extends PureComponent {
+  render() {
+    const {
+      title, handleSubmit, submitButtonProps, inputFields,
+    } = this.props;
+
+    const emailTextInput = (
+      <TextInput
+        name="email"
+        validate={['required', 'email']}
+        type="text"
+        label="Email"
+      />
+    );
+
+    const passwordTextInput = (
+      <TextInput
+        name="password"
+        validate={['required', 'minPasswordLength']}
+        type="password"
+        label="Password"
+      />
+    );
+
+    const inputFieldArray = inputFields.map((field) => {
+      if (field.textInput) {
+        if (field.textInput.name === 'email') {
+          return emailTextInput;
+        }
+        if (field.textInput.name === 'password') {
+          return passwordTextInput;
+        }
+        // Custom field input
+        return <TextInput {...field.textInput} />;
+      }
+      return <SelectInput {...field.selectInput} />;
+    });
+
+    return (
+      <form onSubmit={handleSubmit}>
+        {title && <div className="title">{title}</div> }
+        {inputFieldArray}
+        <Button caption="Submit" type="submit" {...submitButtonProps} />
+      </form>
+    );
+  }
+}
+
+CustomForm.propTypes = {
+  title: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  submitButtonCaption: PropTypes.string,
+  inputFields: PropTypes.array,
+};
+
+export default CustomForm;
